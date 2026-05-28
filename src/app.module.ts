b4d771527f422f4b83e815eb5e36dbcb7fe3fb2d
Ramos <andfera201@gmail.com>
2026-05-27 22:10:05 -0500

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { envValidation } from './config/env.valdiation'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: envValidation,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
